0781b9ac0ec4af386e5aa5ad785bdf3c9735603a
András <asszem78@gmail.com>
2018-07-01 11:57:01 +0200

    Import Bootstrap 4 and jQuery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,6 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    NgbModule.forRoot(), // ng-bootstrap module
     BrowserModule
   ],
   providers: [],
